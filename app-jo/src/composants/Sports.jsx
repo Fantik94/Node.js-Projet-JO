c2fa030aa7bfb0a71f9b31698286d027e9586e52
H3Airness <damienraunier@gmail.com>
2024-03-06 09:29:28 +0100

    update bdd et page sport

--- app-jo/src/composants/Sports.jsx
+++ app-jo/src/composants/Sports.jsx
@@ -25,15 +25,26 @@ function Sports() {
     }, []);
 
     return (
-        <div>
-            <h1>Liste des sports</h1>
+        <div className="container mx-auto px-4">
+            <h1 className="text-4xl font-bold text-gray-800 text-center my-10">Liste des sports</h1>
             {loading ? (
                 <p>Chargement en cours...</p>
             ) : (
                 <ul>
-                    {sports.map(sport => (
-                        <li key={sport.id}>{sport.name_sport}</li>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 my-10">
+                    {sports.map((sport) => (
+                        <div key={sport.id} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden">
+                            <img
+                                src={sport.img_sport}
+                                alt={sport.name_sport}
+                                className="rounded-t-lg object-cover h-48 w-full"
+                            />
+                            <div className="p-5">
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">{sport.name_sport}</h5>
+                            </div>
+                        </div>
                     ))}
+                    </div>
                 </ul>
             )}
         </div>
