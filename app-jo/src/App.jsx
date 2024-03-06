3bbf7e6505d2be634a3a8ef0dccaae880dda69fa
Fantik94 <84931090+Fantik94@users.noreply.github.com>
2024-03-06 07:28:03 +0100

    connexion et context

--- app-jo/src/App.jsx
+++ app-jo/src/App.jsx
@@ -4,10 +4,10 @@ import Navbar from './composants/Navbar';
 import './App.css'
 import Home from './composants/Home';
 import Epreuves from './composants/Epreuves';
+import Connexion from './composants/Connexion';
 
 const Sports = () => <div>Sports Page</div>;
 const SitesOlympiques = () => <div>Sites Olympiques Page</div>;
-const Connexion = () => <div>Connexion Page</div>;
 
 function App() {
   return (
@@ -19,7 +19,7 @@ function App() {
           <Route path="/sports" element={<Sports />} />
           <Route path="/epreuves" element={<Epreuves />} />
           <Route path="/sites" element={<SitesOlympiques />} />
-          <Route path="/connexion" element={<Connexion />} />
+          <Route path="/login" element={<Connexion />} />
         </Routes>
       </div>
     </Router>
