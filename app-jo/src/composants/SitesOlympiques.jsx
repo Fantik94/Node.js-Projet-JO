9396892d0c3bdf848ebe17a9ff186f1d615b9cdb
Hugo <80489889+Bagass0@users.noreply.github.com>
2024-03-06 11:18:45 +0100

    Update SitesOlympiques.jsx

--- app-jo/src/composants/SitesOlympiques.jsx
+++ app-jo/src/composants/SitesOlympiques.jsx
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 function SitesOlympiques() {
     const [sitesOlympiques, setSitesOlympiques] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filteredSites, setFilteredSites] = useState([]);
 
     useEffect(() => {
         const fetchSitesOlympiques = async () => {
@@ -24,33 +26,83 @@ function SitesOlympiques() {
         fetchSitesOlympiques();
     }, []);
 
-    // Créer une structure de données pour stocker les sports par site olympique
-    const sportsParSite = {};
-    sitesOlympiques.forEach(site => {
-        if (!sportsParSite[site.site_olympique]) {
-            sportsParSite[site.site_olympique] = [];
+    useEffect(() => {
+        const uniqueSites = Array.from(new Set(sitesOlympiques.map(site => site.site_olympique)))
+            .map(site_olympique => {
+                return {
+                    site_olympique,
+                    sports: sitesOlympiques.filter(site => site.site_olympique === site_olympique)
+                };
+            });
+        setFilteredSites(uniqueSites);
+    }, [sitesOlympiques]);
+
+    const sportRefs = useRef({});
+
+    const scrollToSport = (sportRef) => {
+        if (sportRef && sportRef.current) {
+            sportRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
         }
-        sportsParSite[site.site_olympique].push({ name: site.name_sport, image: site.img_sport });
-    });
+    };
+
+    // Filtrer les sports en fonction du terme de recherche
+    const filteredSitesBySearchTerm = filteredSites.filter(site =>
+        site.sports.some(sport =>
+            sport.name_sport.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+    );
+
+    useEffect(() => {
+        if (searchTerm !== '') {
+            filteredSitesBySearchTerm.forEach(site => {
+                site.sports.forEach(sport => {
+                    if (!sportRefs.current[sport.name_sport]) {
+                        sportRefs.current[sport.name_sport] = React.createRef();
+                    }
+                });
+            });
+        }
+    }, [filteredSitesBySearchTerm, searchTerm]);
+
+    useEffect(() => {
+        if (searchTerm !== '') {
+            filteredSitesBySearchTerm.forEach(site => {
+                site.sports.forEach(sport => {
+                    if (sport.name_sport.toLowerCase().includes(searchTerm.toLowerCase())) {
+                        scrollToSport(sportRefs.current[sport.name_sport]);
+                    }
+                });
+            });
+        }
+    }, [searchTerm, filteredSitesBySearchTerm]);
 
     return (
         <div className="container mx-auto">
             <h1 className="text-2xl md:text-3xl pl-2 my-2 border-l-4 mt-10 mb-10 font-sans font-bold border-yellow-400 dark:text-gray-500">Sites Olympiques</h1>
+            <div className="mb-6">
+                <input
+                    type="text"
+                    placeholder="Rechercher un sport..."
+                    className="px-4 py-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+            </div>
             {loading ? (
                 <p>Chargement en cours...</p>
             ) : (
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-10">
-                    {Object.entries(sportsParSite).map(([site, sports]) => (
-                        <div key={site} className="border rounded-lg p-4 bg-white shadow-md">
-                            <iframe className="w-full h-40 mb-4 object-cover rounded-lg" src={site} title="Site Olympique"></iframe>
-                            <ul className="list-disc pl-4 overflow-y-auto scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100" style={{ maxHeight: '200px' }}>
-                                {sports.map((sport, index) => (
-                                    <li key={index} className="flex items-center mb-2">
-                                        <img src={sport.image} alt={sport.name} className="w-20 h-30 mr-2" />
-                                        {sport.name}
-                                    </li>
+                    {filteredSitesBySearchTerm.map((site, index) => (
+                        <div key={index} className="border rounded-lg p-4 bg-white shadow-md">
+                            <iframe className="w-full h-40 mb-4 object-cover rounded-lg" src={site.site_olympique} title="Site Olympique"></iframe>
+                            <div className="overflow-y-auto scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100" style={{ maxHeight: '200px' }}>
+                                {site.sports.map((sport, sportIndex) => (
+                                    <div key={sportIndex} className="flex items-center mb-2" ref={sportRefs.current[sport.name_sport]}>
+                                        <img src={sport.img_sport} alt={sport.name_sport} className="w-20 h-20 mr-4 rounded-full" />
+                                        <span className="text-gray-800">{sport.name_sport}</span>
+                                    </div>
                                 ))}
-                            </ul>
+                            </div>
                         </div>
                     ))}
                 </div>
